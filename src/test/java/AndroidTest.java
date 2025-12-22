ba691b5e7b707eca60645ed0eab36f99f48aa150
Tatiana Andreeva <andretat8@gmail.com>
2025-12-22 17:38:58 +0100

    Config and tests for android work

--- src/test/java/AndroidTest.java
+++ src/test/java/AndroidTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.*;
@@ -18,13 +19,16 @@ public class AndroidTest extends TestBase{
     }
 
     @Test
-    void searchSerbiaPage() {
+    void checkPageOpens() {
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Serbia");
         });
-        step("Verify content found", () ->
-                $$(className("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
+        step("Click on correct page", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title")).get(0).click();
+        });
+        step("Verify page is opened", () ->
+                $(id("pcs")).shouldHave(text("Serbia")));
 
     }
 }
--- src/test/java/config/MobileConfig.java
+++ src/test/java/config/MobileConfig.java
@@ -8,13 +8,16 @@ public interface MobileConfig extends Config {
 
     @Key("platform")
     @DefaultValue("android")
+   // @DefaultValue("ios")
     String platform();
 
     @Key("os_version")
-    @DefaultValue("12.0")
+    @DefaultValue("13.0")
+   // @DefaultValue("16")
     String os_version();
 
     @Key("device")
-    @DefaultValue("Samsung Galaxy S22 Ultra")
+    @DefaultValue("Google Pixel 7 Pro")
+   // @DefaultValue("iPhone 14 Pro Max")
     String device();
 }
